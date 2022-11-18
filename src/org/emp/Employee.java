f98648f32bdb75fe8b7ce5344c5c315d8b6a2981
PrabhuDaniel123 <Prabhudanee@gmail.com>
2022-11-18 18:31:10 +0530

    new code

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,36 @@
+package org.emp;
+
+public class Employee {
+private void empID() {
+	System.out.println("111833");
+		
+	}
+public void empName() {
+	System.out.println("Damien");
+	
+}
+private void empDob() {
+ System.out.println("12/12/12");
+ }
+private void empPhone() {
+	System.out.println("1223455678");
+	
+}
+public void empEmail() {
+	System.out.println("sghgdfgsj@gmail.com");
+	
+}
+private void empAddress() {
+	System.out.println("Nodfg jhdfjh kjfkdjfkj");
+	
+}
+public static void main(String[] args) {
+	Employee swatKats =new Employee();
+	swatKats.empID();
+	swatKats.empName();
+	swatKats.empDob();
+    swatKats.empPhone();
+    swatKats.empEmail();
+    swatKats.empAddress();
+}
+}
